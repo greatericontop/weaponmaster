5b0b5cf4810896e72745bdedb712d046136e71df
greateric <greateric@protonmail.com>
2022-05-08 19:14:44 -0400

    atom bomb explosion thingys

--- src/main/java/io/github/greatericontop/weaponmaster/AtomBomb/AtomItemListener.java
+++ src/main/java/io/github/greatericontop/weaponmaster/AtomBomb/AtomItemListener.java
@@ -27,10 +27,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
-import java.util.Deque;
-import java.util.LinkedList;
 import java.util.Random;
 
 public class AtomItemListener implements Listener {
@@ -40,6 +39,22 @@ public class AtomItemListener implements Listener {
     public AtomItemListener(WeaponMasterMain plugin) {
         this.plugin = plugin;
         util = new Util(plugin);
+    }
+
+    public void spawnVein(Location loc, Material type, float size, int limitDepth, Random rnd) {
+        if (loc.getBlock().getType().getBlastResistance() >= 20) { return; }
+        if (loc.getBlock().getType() == Material.AIR) { return; }
+        loc.getBlock().setType(type);
+        limitDepth--;
+        if (limitDepth == 0) {
+            return;
+        }
+        if (rnd.nextFloat() < size)  spawnVein(loc.clone().add(1.0, 0.0, 0.0), type, size, limitDepth, rnd);
+        if (rnd.nextFloat() < size)  spawnVein(loc.clone().add(-1.0, 0.0, 0.0), type, size, limitDepth, rnd);
+        if (rnd.nextFloat() < size)  spawnVein(loc.clone().add(0.0, 1.0, 0.0), type, size, limitDepth, rnd);
+        if (rnd.nextFloat() < size)  spawnVein(loc.clone().add(0.0, -1.0, 0.0), type, size, limitDepth, rnd);
+        if (rnd.nextFloat() < size)  spawnVein(loc.clone().add(0.0, 0.0, 1.0), type, size, limitDepth, rnd);
+        if (rnd.nextFloat() < size)  spawnVein(loc.clone().add(0.0, 0.0, -1.0), type, size, limitDepth, rnd);
     }
 
     @EventHandler(priority = EventPriority.NORMAL)
@@ -66,30 +81,54 @@ public class AtomItemListener implements Listener {
         Location at = event.getBlock().getLocation();
         World world = at.getWorld();
         Random rnd = new Random();
-        for (float deltaX = MIN_VALUE; deltaX <= MAX_VALUE; deltaX += STEP) {
-            for (float deltaY = MIN_VALUE; deltaY <= MAX_VALUE; deltaY += STEP) {
-                for (float deltaZ = MIN_VALUE; deltaZ <= MAX_VALUE; deltaZ += STEP) {
-                    if (!(deltaX <= MIN || deltaX >= MAX || deltaY <= MIN || deltaY >= MAX || deltaZ <= MIN || deltaZ >= MAX)) {
-                        continue;
-                    }
+        new BukkitRunnable() {
+            public void run() {
+                boolean diamondFlag = false;
+                for (float deltaX = MIN_VALUE; deltaX <= MAX_VALUE; deltaX += STEP) {
+                    for (float deltaY = MIN_VALUE; deltaY <= MAX_VALUE; deltaY += STEP) {
+                        for (float deltaZ = MIN_VALUE; deltaZ <= MAX_VALUE; deltaZ += STEP) {
+                            if (!(deltaX <= MIN || deltaX >= MAX || deltaY <= MIN || deltaY >= MAX || deltaZ <= MIN || deltaZ >= MAX)) {
+                                continue;
+                            }
 
-                    Location loc = at.clone();
-                    Vector ray = new Vector(deltaX, deltaY, deltaZ).normalize().multiply(0.6);
-                    float rayPower = 19.0F * (0.9F + 0.2F * rnd.nextFloat());
-                    while (true) {
-                        rayPower -= 0.45F;
-                        if (loc.getBlock().getType() != Material.AIR) {
-                            rayPower -= (loc.getBlock().getType().getBlastResistance() + 0.6F) * 0.6F;
+                            Location loc = at.clone();
+                            Vector ray = new Vector(deltaX, deltaY, deltaZ).normalize().multiply(0.6);
+                            float rayPower = 23.0F * (0.88F + 0.24F * rnd.nextFloat());
+                            while (true) {
+                                rayPower -= 0.45F;
+                                if (loc.getBlock().getType() != Material.AIR) {
+                                    rayPower -= (loc.getBlock().getType().getBlastResistance() + 0.6F) * 0.6F;
+                                }
+                                if (rayPower <= 0) {
+                                    if (loc.getBlock().getType() != Material.AIR) {
+                                        float f = rnd.nextFloat();
+                                        if (f < 0.00_1F) {
+                                            loc.getBlock().setType(Material.COBBLESTONE);
+                                        } else if (f < 0.00_22F) {
+                                            spawnVein(loc, Material.COBBLED_DEEPSLATE, 0.3F, 2, rnd);
+                                        } else if (f < 0.00_32F) {
+                                            loc.getBlock().setType(Material.COBWEB);
+                                        } else if (f < 0.00_38F) {
+                                            loc.getBlock().setType(Material.OBSIDIAN);
+                                        } else if (f < 0.00_65F) {
+                                            loc.getBlock().setType(Material.FIRE);
+                                        }
+                                        if (f > 0.99_7 && loc.getBlock().getType() == Material.COAL_ORE) {
+                                            spawnVein(loc, Material.DEEPSLATE_DIAMOND_ORE, 0.6F, 4, rnd);
+                                            diamondFlag = true;
+                                        }
+                                    }
+                                    break;
+                                }
+                                loc.getBlock().setType(Material.AIR);
+                                loc = loc.add(ray);
+                            }
                         }
-                        if (rayPower <= 0) {
-                            break;
-                        }
-                        loc.getBlock().setType(Material.AIR);
-                        loc = loc.add(ray);
                     }
                 }
+                if (diamondFlag) { player.sendMessage("§3The explosion caused immense pressure and changed the world!"); }
             }
-        }
+        }.runTaskLater(plugin, 1L);
 
 
         player.sendMessage("§6[!] §3You have successfully levelled the landscape.");
