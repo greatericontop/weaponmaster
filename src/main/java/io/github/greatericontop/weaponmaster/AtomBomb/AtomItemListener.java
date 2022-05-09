235b61bbcc1062903c4622f5e8f5de35c75a0df6
greatericontop <greateric@protonmail.com>
2022-05-09 16:54:01 -0400

    more powerful atom bombs and less laggy

--- src/main/java/io/github/greatericontop/weaponmaster/AtomBomb/AtomItemListener.java
+++ src/main/java/io/github/greatericontop/weaponmaster/AtomBomb/AtomItemListener.java
@@ -57,6 +57,19 @@ public class AtomItemListener implements Listener {
         if (rnd.nextFloat() < size)  spawnVein(loc.clone().add(0.0, 0.0, -1.0), type, size, limitDepth, rnd);
     }
 
+    public float getCustomBlastResistance(Material mat) {
+        float blastRes = mat.getBlastResistance();
+        // netherite, obsidian, etc
+        if (blastRes == 1200.0F) {
+            return 20.0F;
+        }
+        // water, lava, bubble column
+        if (blastRes == 100.0F) {
+            return 9.0F; // equivalent to end stone
+        }
+        return blastRes;
+    }
+
     @EventHandler(priority = EventPriority.NORMAL)
     public void onBlockPlace(BlockPlaceEvent event) {
         Player player = event.getPlayer();
@@ -73,31 +86,39 @@ public class AtomItemListener implements Listener {
 
         float MIN_VALUE = -1.0F;
         float MAX_VALUE = 1.0F;
-        float STEP = 1.0F / 64.0F;
-        float MIN = -0.999F;
-        float MAX = 0.999F;
+        float STEP = 1.0F / 192.0F;
+        float MIN = -1.0001F + STEP;
+        float MAX = 1.0001F - STEP;
         // An attempt to copy the minecraft explosion algorithm
         // This is O(scary); but it seems to work decently in practice.
         Location at = event.getBlock().getLocation();
         World world = at.getWorld();
         Random rnd = new Random();
         new BukkitRunnable() {
+            boolean worldChangeFlag = false;
+            float deltaY = MAX_VALUE;
             public void run() {
-                boolean diamondFlag = false;
-                for (float deltaX = MIN_VALUE; deltaX <= MAX_VALUE; deltaX += STEP) {
-                    for (float deltaY = MIN_VALUE; deltaY <= MAX_VALUE; deltaY += STEP) {
+                if (deltaY < MIN_VALUE) {
+                    player.sendMessage("§6[!] §3You have successfully levelled the landscape.");
+                    if (worldChangeFlag) {
+                        player.sendMessage("§eYour explosion has caused immense pressure conditions that changed the world!");
+                    }
+                    cancel();
+                    return;
+                }
+                for (int i = 0; i < 7; i++) {
+                    for (float deltaX = MIN_VALUE; deltaX <= MAX_VALUE; deltaX += STEP) {
                         for (float deltaZ = MIN_VALUE; deltaZ <= MAX_VALUE; deltaZ += STEP) {
                             if (!(deltaX <= MIN || deltaX >= MAX || deltaY <= MIN || deltaY >= MAX || deltaZ <= MIN || deltaZ >= MAX)) {
                                 continue;
                             }
-
                             Location loc = at.clone();
                             Vector ray = new Vector(deltaX, deltaY, deltaZ).normalize().multiply(0.6);
-                            float rayPower = 23.0F * (0.88F + 0.24F * rnd.nextFloat());
+                            float rayPower = 32.0F * (0.8F + 0.4F * rnd.nextFloat());
                             while (true) {
                                 rayPower -= 0.45F;
                                 if (loc.getBlock().getType() != Material.AIR) {
-                                    rayPower -= (loc.getBlock().getType().getBlastResistance() + 0.6F) * 0.6F;
+                                    rayPower -= (getCustomBlastResistance(loc.getBlock().getType()) + 0.6F) * 0.6F;
                                 }
                                 if (rayPower <= 0) {
                                     if (loc.getBlock().getType() != Material.AIR) {
@@ -108,14 +129,14 @@ public class AtomItemListener implements Listener {
                                             spawnVein(loc, Material.COBBLED_DEEPSLATE, 0.2F, 2, rnd);
                                         } else if (f < 0.00_32F) {
                                             loc.getBlock().setType(Material.COBWEB);
-                                        } else if (f < 0.00_38F) {
-                                            loc.getBlock().setType(Material.OBSIDIAN);
+                                        } else if (f < 0.00_36F) {
+                                            spawnVein(loc, Material.OBSIDIAN, 0.1F, 2, rnd);
                                         } else if (f < 0.00_65F) {
                                             loc.getBlock().setType(Material.FIRE);
                                         }
                                         if (f > 0.99_7 && loc.getBlock().getType() == Material.COAL_ORE) {
                                             spawnVein(loc, Material.DEEPSLATE_DIAMOND_ORE, 0.45F, 4, rnd);
-                                            diamondFlag = true;
+                                            worldChangeFlag = true;
                                         }
                                     }
                                     break;
@@ -125,13 +146,10 @@ public class AtomItemListener implements Listener {
                             }
                         }
                     }
+                    deltaY -= STEP;
                 }
-                if (diamondFlag) { player.sendMessage("§3The explosion caused immense pressure and changed the world!"); }
             }
-        }.runTaskLater(plugin, 1L);
-
-
-        player.sendMessage("§6[!] §3You have successfully levelled the landscape.");
+        }.runTaskTimer(plugin, 1L, 1L);
     }
 
 }
