8f5d74c3e1cbdddca5267a7c7ede32c3ff12327f
greatericontop <greateric@protonmail.com>
2022-05-10 17:07:48 -0400

    adding tiers

--- src/main/java/io/github/greatericontop/weaponmaster/MinerBlessing/MinerItemListener.java
+++ src/main/java/io/github/greatericontop/weaponmaster/MinerBlessing/MinerItemListener.java
@@ -20,21 +20,27 @@ package io.github.greatericontop.weaponmaster.MinerBlessing;
 import io.github.greatericontop.weaponmaster.WeaponMasterMain;
 import io.github.greatericontop.weaponmaster.utils.Util;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.List;
+import java.util.Random;
 
 public class MinerItemListener implements Listener {
 
     private final WeaponMasterMain plugin;
     private final Util util;
+    private final Random rnd = new Random();
     public MinerItemListener(WeaponMasterMain plugin) {
         this.plugin = plugin;
         util = new Util(plugin);
@@ -52,26 +58,38 @@ public class MinerItemListener implements Listener {
     }
 
     private int getRequirementToLevelUp(int level) {
-        if (level >= 6) {
+        if (level >= 8) {
             return 2147483600;
         }
         return new int[]{
                 10_000,
                 15_000, // 1
-                25_000,
+                20_000,
+                30_000,
                 40_000,
+                50_000, // 5
                 60_000,
-                90_000, // 5
+                70_000,
         }[level];
+//        return new int[]{
+//                10_000,
+//                15_000, // 1
+//                25_000,
+//                40_000,
+//                60_000,
+//                90_000, // 5
+//                135_000,
+//                180_000,
+//        }[level];
     }
 
     public int xpToAdd(Material mat) {
-        if (mat == Material.DEEPSLATE_COAL_ORE)  return 2100;
-        if (mat == Material.DEEPSLATE_EMERALD_ORE || mat == Material.DEEPSLATE_DIAMOND_ORE)  return 1000;
-        if (mat == Material.EMERALD_ORE || mat == Material.DIAMOND_ORE)  return 840;
-        if (mat == Material.DEEPSLATE_IRON_ORE || mat == Material.DEEPSLATE_REDSTONE_ORE || mat == Material.DEEPSLATE_GOLD_ORE || mat == Material.DEEPSLATE_LAPIS_ORE)  return 610;
-        if (mat == Material.COAL_ORE || mat == Material.IRON_ORE || mat == Material.REDSTONE_ORE || mat == Material.GOLD_ORE || mat == Material.LAPIS_ORE)  return 400;
-        if (mat == Material.OBSIDIAN || mat == Material.ANCIENT_DEBRIS)  return 50;
+        if (mat == Material.DEEPSLATE_COAL_ORE)  return 2700;
+        if (mat == Material.DEEPSLATE_EMERALD_ORE || mat == Material.DEEPSLATE_DIAMOND_ORE)  return 1500;
+        if (mat == Material.EMERALD_ORE || mat == Material.DIAMOND_ORE)  return 1000;
+        if (mat == Material.DEEPSLATE_IRON_ORE || mat == Material.DEEPSLATE_REDSTONE_ORE || mat == Material.DEEPSLATE_GOLD_ORE || mat == Material.DEEPSLATE_LAPIS_ORE)  return 810;
+        if (mat == Material.COAL_ORE || mat == Material.IRON_ORE || mat == Material.REDSTONE_ORE || mat == Material.GOLD_ORE || mat == Material.LAPIS_ORE)  return 600;
+        if (mat == Material.OBSIDIAN || mat == Material.ANCIENT_DEBRIS)  return 70;
         if (mat == Material.DEEPSLATE)  return 15;
         if (mat == Material.STONE)  return 6;
         if (mat == Material.NETHERRACK) return 2;
@@ -117,9 +135,14 @@ public class MinerItemListener implements Listener {
                 im.addEnchant(Enchantment.DAMAGE_ALL, 3, true);
                 lore.set(util.MINER_INSERTION, "§eEfficiency V, Unbreaking III, Sharpness III");
                 break;
+            case 7:
+                lore.add(util.MINER_INSERTION, "§3RIGHT CLICK to cycle between Silk Touch and Fortune III.");
+                break;
+            case 8:
+                lore.add(util.MINER_INSERTION, "§aAutomatically smelts some ores and drops additional experience.");
+                break;
         }
     }
-
 
     @EventHandler(priority = EventPriority.NORMAL)
     public void onBlockBreak(BlockBreakEvent event) {
@@ -136,7 +159,6 @@ public class MinerItemListener implements Listener {
         int exp = parseExpInt(lore.get(util.MINER_EXP));
         int tier = parseLevelInt(lore.get(util.MINER_LVL));
         exp += amount;
-
         if (exp >= getRequirementToLevelUp(tier)) {
             exp = 0;
             tier++;
@@ -148,7 +170,57 @@ public class MinerItemListener implements Listener {
         double xpPercent = (100.0 * exp) / getRequirementToLevelUp(tier);
         lore.set(util.MINER_REQ, String.format("§6Required: §b%d §6(§b%.1f§6%%)", getRequirementToLevelUp(tier), xpPercent));
 
+        if (tier >= 8) {
+            doSmeltingOres(event, player);
+        }
+
         im.setLore(lore);
+        player.getInventory().getItemInMainHand().setItemMeta(im);
+    }
+
+    public void doSmeltingOres(BlockBreakEvent event, Player player) {
+        Material mat = event.getBlock().getType();
+        World world = event.getBlock().getLocation().getWorld();
+        if (mat == Material.COPPER_ORE || mat == Material.DEEPSLATE_COPPER_ORE) {
+            event.setDropItems(false);
+            int amount = rnd.nextInt(3) + 2;
+            world.dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.COPPER_INGOT, amount));
+            event.setExpToDrop(3*amount);
+        } else if (mat == Material.IRON_ORE || mat == Material.DEEPSLATE_IRON_ORE) {
+            event.setDropItems(false);
+            int amount = 1;
+            world.dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.IRON_INGOT, amount));
+            event.setExpToDrop(3*amount);
+        } else if (mat == Material.GOLD_ORE || mat == Material.DEEPSLATE_GOLD_ORE) {
+            event.setDropItems(false);
+            int amount = 1;
+            world.dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.GOLD_INGOT, amount));
+            event.setExpToDrop(5*amount);
+        } else if (mat == Material.ANCIENT_DEBRIS) {
+            event.setDropItems(false);
+            world.dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.NETHERITE_SCRAP, 1));
+            event.setExpToDrop(14);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onRightClick(PlayerInteractEvent event) {
+        if (event.getHand() != EquipmentSlot.HAND) { return; }
+        Player player = event.getPlayer();
+        if (!util.checkForMinersBlessing(player.getInventory().getItemInMainHand())) { return; }
+        if (!player.hasPermission("weaponmaster.minersblessing.use")) {
+            player.sendMessage("§3Sorry, you cannot use this item yet. You need the permission §4weaponmaster.minersblessing.use§3.");
+            return;
+        }
+        if (!(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)) { return; }
+        ItemMeta im = player.getInventory().getItemInMainHand().getItemMeta();
+        List<String> lore = im.getLore();
+        int tier = parseLevelInt(lore.get(util.MINER_LVL));
+        if (tier < 7) { return; }
+
+        player.sendMessage("§7this is a test, adding you fortune");
+        im.addEnchant(Enchantment.LOOT_BONUS_BLOCKS, 3, false);
+
         player.getInventory().getItemInMainHand().setItemMeta(im);
     }
 
