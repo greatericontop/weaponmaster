452edd22473c08ab9809c57b70e2251576250b46
gerseneck <wang14240@gmail.com>
2022-05-15 19:39:14 -0400

    no mo ocd for me

--- src/main/java/io/github/greatericontop/weaponmaster/utils/Util.java
+++ src/main/java/io/github/greatericontop/weaponmaster/utils/Util.java
@@ -287,7 +287,7 @@ public class Util {
         NETHERITE_STAFF_LORE.add("§3Gives random effects to opponent when hit.");
         NETHERITE_STAFF_LORE.add("§3Effects can be good or bad.");
         NETHERITE_STAFF_LORE.add("");
-        NETHERITE_STAFF_LORE.add("§eRIGHT CLICK §3 Shoots an arrow with random effect.");
+        NETHERITE_STAFF_LORE.add("§eRIGHT CLICK §3Shoots an arrow with random effect.");
         NETHERITE_STAFF_LORE.add("");
         NETHERITE_STAFF_LORE.add("§cSUPREME");
         NETHERITE_STAFF_LORE.add("§6---------------");
