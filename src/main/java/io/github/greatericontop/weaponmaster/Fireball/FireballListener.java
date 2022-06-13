fd26afbd39f6389dc19d52e9c4472e74655ede69
Gerseneck <wang14240@gmail.com>
2022-06-12 20:31:45 -0400

    fixed this stupid setting

--- src/main/java/io/github/greatericontop/weaponmaster/Fireball/FireballListener.java
+++ src/main/java/io/github/greatericontop/weaponmaster/Fireball/FireballListener.java
@@ -19,8 +19,14 @@ package io.github.greatericontop.weaponmaster.Fireball;
 
 import io.github.greatericontop.weaponmaster.WeaponMasterMain;
 import io.github.greatericontop.weaponmaster.utils.Util;
-import org.bukkit.*;
-import org.bukkit.entity.*;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.World;
+import org.bukkit.entity.DragonFireball;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Fireball;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
