95aafeb1c881ee8cc22690598b2d5cf48927ab3b
Gerseneck <wang14240@gmail.com>
2023-05-26 10:12:46 -0400

    refactor TridentListener

--- src/main/java/io/github/greatericontop/weaponmaster/mainitems/PoseidonTrident/TridentListener.java
+++ src/main/java/io/github/greatericontop/weaponmaster/mainitems/PoseidonTrident/TridentListener.java
@@ -21,20 +21,13 @@ import io.github.greatericontop.weaponmaster.WeaponMasterMain;
 import io.github.greatericontop.weaponmaster.utils.Util;
 import org.bukkit.Bukkit;
 import org.bukkit.Particle;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.AbstractArrow;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Trident;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -56,9 +49,6 @@ public class TridentListener implements Listener {
                     if (!player.hasPermission("weaponmaster.poseidontrident.use")) { continue; }
                     if (!util.checkForPoseidonTrident(player.getInventory().getItemInMainHand())) { continue; }
                     player.addPotionEffect(new PotionEffect(PotionEffectType.CONDUIT_POWER, 39, 0));
-                    ItemStack it = player.getInventory().getItemInMainHand();
-                    ItemMeta im = it.getItemMeta();
-                    it.setItemMeta(im);
                     if (player.isInWater() && Math.random() < 0.015) {
                         player.addPotionEffect(new PotionEffect(PotionEffectType.DOLPHINS_GRACE, 39, 0));
                     }
@@ -76,7 +66,6 @@ public class TridentListener implements Listener {
             player.sendMessage("§3Sorry, you cannot use this item yet. You need the permission §4weaponmaster.poseidontrident.use§3.");
             return;
         }
-        player.removePotionEffect(PotionEffectType.CONDUIT_POWER);
         player.getWorld().spawnParticle(Particle.ELECTRIC_SPARK, player.getEyeLocation().add(player.getEyeLocation().getDirection().multiply(0.9)), 15);
     }
 
