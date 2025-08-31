3b0f945042b23624386abc24704ae226633b652b
greatericontop <greateric@protonmail.com>
2025-08-31 10:29:16 -0400

    fix helios kill credit

--- src/main/java/io/github/greatericontop/weaponmaster/mainitems/Helios/HeliosItemListener.java
+++ src/main/java/io/github/greatericontop/weaponmaster/mainitems/Helios/HeliosItemListener.java
@@ -103,7 +103,7 @@ public class HeliosItemListener implements Listener {
             }
             LivingEntity target = (LivingEntity) e;
             double damage = 5.0 + helios.getItemMeta().getEnchantLevel(Enchantment.SHARPNESS) * 0.25;
-            target.damage(damageMultiply(player, damage));
+            target.damage(damageMultiply(player, damage), player);
             target.setFireTicks(target.getFireTicks() + 200);
             target.setVelocity(target.getVelocity().add(new Vector(0.0, 0.4, 0.0)));
         }
