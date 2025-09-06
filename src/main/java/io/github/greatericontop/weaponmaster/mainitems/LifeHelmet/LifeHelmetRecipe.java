732933fc75af11a543cb84a1d68390e8b01bb3f2
greatericontop <greateric@protonmail.com>
2025-09-06 18:45:14 -0400

    made life helmet recipe a bit easier to obtain

--- src/main/java/io/github/greatericontop/weaponmaster/mainitems/LifeHelmet/LifeHelmetRecipe.java
+++ src/main/java/io/github/greatericontop/weaponmaster/mainitems/LifeHelmet/LifeHelmetRecipe.java
@@ -25,9 +25,6 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.RecipeChoice;
 import org.bukkit.inventory.ShapedRecipe;
-import org.bukkit.inventory.meta.PotionMeta;
-import org.bukkit.potion.PotionData;
-import org.bukkit.potion.PotionType;
 
 public class LifeHelmetRecipe {
 
@@ -41,25 +38,11 @@ public class LifeHelmetRecipe {
     public void regRecipe() {
         ItemStack lifeHelmet = util.generateMeta(util.LIFE_HELMET_LORE, util.LIFE_HELMET_NAME, Material.DIAMOND_HELMET);
         ShapedRecipe recipe = new ShapedRecipe(NamespacedKey.minecraft("life_helmet"), lifeHelmet);
-        recipe.shape("dnd",
+        recipe.shape("ddd",
                      "dCd",
-                     "hTr");
+                     "TTT");
         recipe.setIngredient('d', Material.DIAMOND_BLOCK);
-        recipe.setIngredient('n', Material.NETHERITE_BLOCK);
         recipe.setIngredient('T', Material.TOTEM_OF_UNDYING);
-
-        ItemStack healPotion = new ItemStack(Material.LINGERING_POTION, 1);
-        PotionMeta im1 = (PotionMeta) healPotion.getItemMeta();
-        im1.setBasePotionData(new PotionData(PotionType.HEALING, false, true));
-        healPotion.setItemMeta(im1);
-        recipe.setIngredient('h', new RecipeChoice.ExactChoice(healPotion));
-
-        ItemStack regenPotion = new ItemStack(Material.LINGERING_POTION, 1);
-        PotionMeta im2 = (PotionMeta) regenPotion.getItemMeta();
-        im2.setBasePotionData(new PotionData(PotionType.REGENERATION, true, false));
-        regenPotion.setItemMeta(im2);
-        recipe.setIngredient('r', new RecipeChoice.ExactChoice(regenPotion));
-
         recipe.setIngredient('C', new RecipeChoice.ExactChoice(customItems.generateLifeCoreItemStack()));
         Bukkit.getServer().addRecipe(recipe);
     }
