f00d1479f693f27be6c936e562cb5ba857c96cf4
greatericontop <greateric@protonmail.com>
2025-10-15 16:39:07 -0400

    change excalibur damage to 2.5 hearts per 6s

--- src/main/java/io/github/greatericontop/weaponmaster/mainitems/Excalibur/ExcaliburItemListener.java
+++ src/main/java/io/github/greatericontop/weaponmaster/mainitems/Excalibur/ExcaliburItemListener.java
@@ -48,16 +48,15 @@ public class ExcaliburItemListener implements Listener {
     @EventHandler()
     public void onDamageByEntity(EntityDamageByEntityEvent event) {
         if (event.getDamager().getType() != EntityType.PLAYER)  return;
-        Player player = (Player)event.getDamager();
-        if (!util.checkForExcalibur(player.getInventory().getItemInMainHand())) { return; }
+        Player player = (Player) event.getDamager();
+        if (!util.checkForExcalibur(player.getInventory().getItemInMainHand()))  return;
         if (!player.hasPermission("weaponmaster.excalibur.use")) {
             player.sendMessage("§3Sorry, you cannot use this item yet. You need the permission §4weaponmaster.excalibur.use§3.");
             return;
         }
-        if (!(event.getEntity() instanceof LivingEntity)) {
+        if (!(event.getEntity() instanceof LivingEntity victim)) {
             return;
         }
-        LivingEntity victim = (LivingEntity) event.getEntity();
         if (cooldowns.getOrDefault(player.getUniqueId(), true)) {
             new BukkitRunnable() {
                 public void run() {
@@ -67,7 +66,7 @@ public class ExcaliburItemListener implements Listener {
                     }
                     victim.getWorld().createExplosion(victim.getLocation(), 0.0F);
                     victim.getWorld().spawnParticle(Particle.EXPLOSION, victim.getLocation(), 4);
-                    TrueDamageHelper.dealTrueDamage(victim, 3.0); // dealing true damage does NOT require no damage ticks
+                    TrueDamageHelper.dealTrueDamage(victim, 5.0); // dealing true damage does NOT require no damage ticks
                 }
             }.runTaskLater(plugin, 1L);
             cooldowns.put(player.getUniqueId(), false);
